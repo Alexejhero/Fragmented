e142c73ac72b7f341ca508e55b19943fb4b6b9f6
chris <chris@summit-tech.ca>
2024-12-28 11:35:25 -0500

    sample neuro animation tree
    in builder scene, neuro animations can be tested using WASD keys
    -i think sadecs included movement within the animation, which we likely won't want

---
+++ Assets/Art/Characters/Scripts/SampleController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+/// <summary>
+/// For now I'll use this just to test crude movement and animations. Planning to sync with Govo to reuse our old controller
+/// </summary>
+public class SampleController : MonoBehaviour
+{
+	[SerializeField]
+	private Animator characterAnimator;
+
+	private bool isMoving = false;
+	private bool isLeft = true;
+
+	private void Awake()
+	{
+		//characterAnimator = GetComponentInChildren<Animator>();
+	}
+
+	private void Update()
+	{
+		isMoving = false;
+		if (UnityEngine.Input.GetKey(KeyCode.A))
+		{
+			isLeft = true;
+			isMoving = true;
+		}
+		if (UnityEngine.Input.GetKey(KeyCode.D))
+		{
+			isLeft = false;
+			isMoving = true;
+		}
+		if (UnityEngine.Input.GetKey(KeyCode.W))
+		{
+			isMoving = true;
+		}
+		if (UnityEngine.Input.GetKey(KeyCode.S))
+		{
+			isMoving = true;
+		}
+		Debug.Log("mov " + isMoving + "  ?? " + isLeft);
+		characterAnimator.SetBool("isLeft", isLeft);
+		characterAnimator.SetBool("isMoving", isMoving);
+	}
+
+	private void PlayerReset()
+	{
+		characterAnimator.SetBool("isLeft", true);
+		characterAnimator.SetBool("isMoving", false);
+	}
+
+}
