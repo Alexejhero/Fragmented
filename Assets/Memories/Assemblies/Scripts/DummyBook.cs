13cfe50fcc8f16b63f75bebf36234130bd01387f
chris <chris@summit-tech.ca>
2024-12-28 13:47:48 -0500

    dummy book crude

---
+++ Assets/Memories/Assemblies/Scripts/DummyBook.cs
@@ -0,0 +1,39 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Memories.Book
+{
+	public class DummyBook : MonoBehaviour
+	{
+		[SerializeField]
+		private Popup[] popups;
+
+		[SerializeField]
+		private float openTime=2f;
+
+		private void Awake()
+		{
+			popups = GetComponentsInChildren<Popup>();
+		}
+
+		private void UpdatePopups(float lift)
+		{
+			foreach (Popup mp in popups)
+			{
+				mp.DoRotate(lift);
+			}
+		}
+
+		private async UniTask DummyOpen()
+		{
+			float lerpval = 0;
+			while (lerpval < 1)
+			{
+				lerpval += Time.deltaTime / openTime;
+				UpdatePopups(lerpval);
+				await UniTask.Yield();
+			}
+		}
+	}
+}
