1e521829cb42be5c07ec9f603bd04d655e16c304
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-12-28 09:22:47 +0200

    Book in bookshelf

--- Assets/Memories/Book/MemoryBook.cs
+++ Assets/Memories/Book/MemoryBook.cs
@@ -5,13 +5,26 @@ namespace Memories.Book
     [AddComponentMenu("Memory/Book")]
     public class MemoryBook : MonoBehaviour
     {
+        private static readonly int _openProp = Animator.StringToHash("open");
+
+        private Animator _animator;
+
         public GameObject left;
         public GameObject right;
 
         public float pageSeparation;
 
+        public bool open;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
         private void Update()
         {
+            _animator.SetBool(_openProp, open);
+
             float absDiff = Mathf.Abs(left.transform.localEulerAngles.y - right.transform.localEulerAngles.y);
             float degreeDiff = absDiff % 360;
             const float ONE_OVER_180 = 1f / 180f; // multiplication is faster than division;
