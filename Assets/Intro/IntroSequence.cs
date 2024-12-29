b8282d7e277da81387ce49547fa132b2e7b7fbd5
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-12-29 20:19:58 +0200

    Animate neuro text in intro

--- Assets/Intro/IntroSequence.cs
+++ Assets/Intro/IntroSequence.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,6 +11,7 @@ public class IntroSequence : MonoBehaviour
     public TextMeshProUGUI startText;
     public TextMeshProUGUI neuroText;
 
+    public RawImage videoGraphic;
     public Image videoHider;
 
     public VideoPlayer player;
@@ -19,6 +21,8 @@ public class IntroSequence : MonoBehaviour
     // ReSharper disable once Unity.IncorrectMethodSignature UnusedMember.Global
     public async UniTask Start()
     {
+        neuroText.maxVisibleCharacters = 0;
+
         player.Prepare();
 
         await UniTask.WaitUntil(() => Input.anyKeyDown);
@@ -27,17 +31,32 @@ public class IntroSequence : MonoBehaviour
 
         await UniTask.Delay(3000);
 
-        // player.Play();
-        // videoGraphic.FadeAlpha(1, 1f).Forget();
+        player.Play();
+        videoGraphic.FadeAlpha(1, 1f).Forget();
 
-        // await UniTask.WaitUntil(() => player.time >= player.length - 1.5f);
+        await UniTask.WaitUntil(() => player.time >= player.length - 1.5f);
         videoHider.FadeAlpha(1, 1.5f).Forget();
+        DOVirtual.Float(player.GetDirectAudioVolume(0), 0, 1.5f, volume => player.SetDirectAudioVolume(0, volume));
 
-        // await UniTask.Delay(2500);
+        await UniTask.Delay(2500);
 
         await logger.Run();
 
-        // run neuro dialogue
+        await UniTask.Delay(1000);
+
+        foreach (char c in neuroText.text)
+        {
+            neuroText.maxVisibleCharacters++;
+            await UniTask.Delay(c switch
+            {
+                '\n' => 1000,
+                ',' => 250,
+                ' ' => 100,
+                _ => 50
+            });
+        }
+
+        await UniTask.Delay(1000);
 
         logger.text.FadeAlpha(0, 1f).Forget();
         neuroText.FadeAlpha(0, 1f).Forget();
