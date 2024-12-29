ca8a545312b1ff2fb5133f796cbbb235564c494f
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-12-30 00:55:23 +0200

    Bring neuro back in the book

---
+++ Assets/Memories/Book/SafePositionPopup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Memories.Book;
+
+public class SafePositionPopup : BasePopup
+{
+    public float leftThreshold = -0.95f;
+    public float rightThreshold = 0.95f;
+
+    private Vector3 _startLocalPosition;
+
+    private void Awake()
+    {
+        _startLocalPosition = transform.localPosition;
+    }
+
+    public override void DoRotate(float lift)
+    {
+        if (lift < leftThreshold || lift > rightThreshold) return;
+
+        lift = Mathf.Abs(lift);
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _startLocalPosition, lift);
+    }
+}
