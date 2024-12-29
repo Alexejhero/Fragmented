a95bec8744d7a72d3f91faccb6f719590019c370
chris <chris@summit-tech.ca>
2024-12-28 19:53:54 -0500

    ceiling yo

--- Assets/Memories/Book/MemoryCeiling.cs
+++ Assets/Memories/Book/MemoryCeiling.cs
@@ -16,11 +16,16 @@ namespace Memories.Book
 		private Vector3 start;
 		private Vector3 end;
 
+		private Renderer[] _rends;
+
 		private void Awake()
 		{
 			start = transform.position;
 			end = start;
 			end.y -= dropQuantity;
+
+			_rends = GetComponentsInChildren<Renderer>();
+			ToggleRend(false);
 		}
 
 		public void DoDrop()
@@ -30,12 +35,21 @@ namespace Memories.Book
 
 		private async UniTask DropCeiling(float lerpstart = 0)
 		{
+			ToggleRend(true);
 			float lerpval = lerpstart;
 			while (lerpval < 1)
 			{
 				lerpval += Time.deltaTime / dropTime;
 				transform.position = Vector3.Slerp(start, end, lerpval);
 				await UniTask.Yield();
+			}
+		}
+
+		private void ToggleRend(bool toggle = true)
+		{
+			foreach (Renderer mr in _rends)
+			{
+				mr.enabled = toggle;
 			}
 		}
 	}
