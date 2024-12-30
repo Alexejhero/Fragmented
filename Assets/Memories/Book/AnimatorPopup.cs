ff2625d4340cc0fc43379bb92b3a4f5be7e04394
chris <chris@summit-tech.ca>
2024-12-29 20:40:51 -0500

    Book 5 Airplane (Page 1)
    -Implemented Animator Popup

---
+++ Assets/Memories/Book/AnimatorPopup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Memories.Book;
+
+public class AnimatorPopup : BasePopup
+{
+    public float leftThreshold = -0.1f;
+    public float rightThreshold = 0.1f;
+
+	[SerializeField]
+	private string animatorParam = "isFolded";
+
+    private Animator _animator;
+
+	private void Awake()
+	{
+        _animator = GetComponent<Animator>();
+	}
+
+	public override void DoRotate(float lift)
+    {
+		bool setvalue = lift < leftThreshold || lift > rightThreshold;
+		_animator.SetBool(animatorParam, !setvalue);
+    }
+}
