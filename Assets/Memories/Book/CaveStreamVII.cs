f39d36e5ead1812d7bb40c6f6ca84ae2ab4387e4
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-12-30 12:36:37 +0200

    Add thingy in cave stream

---
+++ Assets/Memories/Book/CaveStreamVII.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Memories.Book;
+
+public class CaveStreamVII : BasePopup
+{
+    public Vector3 zeroLocalPos;
+    public Vector3 oneLocalPos;
+
+    public override void DoRotate(float lift)
+    {
+        lift = Mathf.Abs(lift);
+        transform.localPosition = Vector3.Lerp(zeroLocalPos, oneLocalPos, lift);
+    }
+}
