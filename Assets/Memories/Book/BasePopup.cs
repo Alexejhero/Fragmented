53ef7feeb1b9fe1a5e66295c88c755fd9baf8e7e
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-12-30 04:46:12 +0200

    Actually fix neuro collapsing

--- Assets/Memories/Book/BasePopup.cs
+++ Assets/Memories/Book/BasePopup.cs
@@ -4,5 +4,10 @@ namespace Memories.Book;
 
 public abstract class BasePopup : MonoBehaviour
 {
+    protected void Start()
+    {
+        DoRotate(0);
+    }
+
     public abstract void DoRotate(float lift);
 }
--- Assets/Memories/Book/NeuroPopup.cs
+++ Assets/Memories/Book/NeuroPopup.cs
@@ -1,4 +1,5 @@
-﻿using Memories.Characters.Movement;
+﻿using System;
+using Memories.Characters.Movement;
 using UnityEngine;
 
 namespace Memories.Book;
@@ -15,11 +16,19 @@ public class NeuroPopup : BasePopup
     public bool invertCollapse;
 
     private float _lastLift = 0;
+    private bool _initialized;
+
+    private void Awake()
+    {
+        if (!leftPage || !rightPage) throw new Exception("Left and right pages must be assigned to the spread where neuro is on!");
+    }
 
     private void Update()
     {
-        if (Mathf.Approximately(_lastLift, -1) || Mathf.Approximately(_lastLift, 1))
+        if (Mathf.Approximately(Mathf.Abs(_lastLift), 1))
         {
+            _initialized = true;
+
             popup.reverseRotation = (transform.localPosition.y > bookMidpoint) ^ invertCollapse;
 
             if (!groundTracker.surfaceCollider) return;
@@ -27,10 +36,19 @@ public class NeuroPopup : BasePopup
             if (groundTracker.surfaceCollider.transform.parent.name.Contains("left")) transform.SetParent(leftPage, true);
             else if (groundTracker.surfaceCollider.transform.parent.name.Contains("right")) transform.SetParent(rightPage, true);
         }
+        else if (_initialized)
+        {
+            if (Mathf.Abs(transform.localPosition.x) < 0.3f)
+            {
+                transform.SetParent(rightPage, true);
+                transform.localPosition = new Vector3(Mathf.Lerp(transform.localPosition.x, 0.3f, Time.deltaTime), transform.localPosition.y, transform.localPosition.z);
+            }
+        }
     }
 
     public override void DoRotate(float lift)
     {
+        Debug.Log(lift);
         _lastLift = lift;
     }
 }
